0db4ce66de1991c0ed4ada298cdcdb6c5d77d37f
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2019-05-10 19:36:41 +0900

    ScriptableObjectで読み込む時に譜面情報を読み込んでみる

--- files/program/MusicGame_ TrialVersion/Assets/Scripts/ScriptableObject/MusicData.cs
+++ files/program/MusicGame_ TrialVersion/Assets/Scripts/ScriptableObject/MusicData.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEditor;
 
 [CreateAssetMenu(menuName = "MusicGame/Create MusicData", fileName = "_MusicData", order =0)]
 [System.Serializable]
@@ -10,7 +12,13 @@ public class MusicData : ScriptableObject
 
     public void Insert()
     {
-        musicSeets[0] = null;
-        //出来てる
+        string filePath = AssetDatabase.GetAssetPath(this);
+        string folderPath = Path.GetDirectoryName(filePath);
+        DirectoryInfo dir = new DirectoryInfo(folderPath);
+        FileInfo[] files = dir.GetFiles("*.txt");
+        foreach(FileInfo info in files)
+        {
+            TextAsset data = 
+        }
     }
 }
