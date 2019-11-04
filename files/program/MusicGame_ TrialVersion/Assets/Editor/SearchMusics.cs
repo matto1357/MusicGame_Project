21ed6d9e50400ce4d44d6e61d539cef2a1757082
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2019-11-04 15:15:10 +0900

    ノーツコンバーター

---
+++ files/program/MusicGame_ TrialVersion/Assets/Editor/SearchMusics.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[CustomEditor(typeof(Musics))]
+public class SearchMusics : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Musics musics = target as Musics;
+
+        if (GUILayout.Button("AutoSearch"))
+        {
+            musics.musicDatas = Search(musics);
+
+            //保存
+            EditorUtility.SetDirty(musics);
+            AssetDatabase.SaveAssets();
+        }
+    }
+
+    public List<MusicData> Search(Musics musics)
+    {
+        List<MusicData> datas = new List<MusicData>();
+        Queue<string> directories = new Queue<string>();
+
+        string filePath = AssetDatabase.GetAssetPath(musics);
+        string folderPath = Path.GetDirectoryName(filePath);
+        directories.Enqueue(folderPath);
+        
+        while (directories.Count > 0)
+        {
+            string dir = directories.Dequeue();
+
+            string[] files = Directory.GetFiles(dir);
+            foreach(string file in files)
+            {
+                if(Path.GetExtension(file) == ".asset")
+                {
+                    try
+                    {
+                        MusicData data = AssetDatabase.LoadAssetAtPath(file, typeof(MusicData)) as MusicData;
+                        if(data != null)
+                        {
+                            datas.Add(data);
+                        }
+                    }
+                    catch
+                    {
+                        
+                    }
+                }
+            }
+
+            string[] dirs = Directory.GetDirectories(dir);
+            foreach(string str in dirs)
+            {
+                directories.Enqueue(str);
+            }
+        }
+        return datas;
+    }
+}
---
+++ files/program/MusicGame_ TrialVersion/Assets/Scripts/NotesConverter.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[CustomEditor(typeof(ConvertSetting))]//拡張対象クラス
+public class NotesConverter : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ConvertSetting convertSetting = target as ConvertSetting;
+
+        if(GUILayout.Button("Convert"))
+        {
+            TextAsset textAsset = convertSetting.seetSource;
+            DataType type = convertSetting.targetDataType;
+            Convert(textAsset, type);
+        }
+    }
+
+    private GameObject obj;
+
+    public void Convert(TextAsset textAsset, DataType type)
+    {
+        obj = new GameObject();
+        MusicManager manager = obj.AddComponent<MusicManager>();
+        manager.MusicLoad(textAsset, LoadMode.PreLoad);
+        if (manager.data_DataType == type)
+        {
+            Debug.LogError("変換元と変換先のDataTypeが一致しています");
+            EditorApplication.delayCall += DestroyObj;
+            return;
+        }
+
+        string filePath = AssetDatabase.GetAssetPath(textAsset);
+        string folderPath = Path.GetDirectoryName(filePath);
+        string path = Directory.GetParent(UnityEngine.Application.dataPath) + "/" + folderPath + "/converted.txt";
+        using (File.Create(path)) { };
+        StreamWriter sw = new StreamWriter(path);
+
+        bool cont = true;
+        int[] laneIndex = new int[manager.data_MusicScore.Length];
+        while(cont)
+        {
+            cont = false;
+            NotesInfo info = null;
+            int tgt = 0;
+
+            for(int lane = 0; lane < manager.data_MusicScore.Length; lane++)
+            {
+                if(manager.data_MusicScore[lane].Count == laneIndex[lane])
+                {
+                    continue;
+                }
+                
+                if(info == null)
+                {
+                    info = manager.data_MusicScore[lane][laneIndex[lane]];
+                    tgt = lane;
+                    cont = true;
+                }
+                else
+                {
+                    float info_time = (float)info.bar + (float)info.th / (float)info.barOriginTh;
+                    NotesInfo tgtInfo = manager.data_MusicScore[lane][laneIndex[lane]];
+                    float tgt_time = (float)tgtInfo.bar + (float)tgtInfo.th / (float)tgtInfo.barOriginTh;
+                    if(tgt_time < info_time)
+                    {
+                        info = tgtInfo;
+                        tgt = lane;
+                    }
+                }
+            }
+            if(cont)
+            {
+                string str = "[" + tgt + "," + ((int)info.type) + "," + info.bar + "." + info.th + "/" + info.barOriginTh + "]";
+                sw.WriteLine(str);
+                laneIndex[tgt]++;
+            }
+        }
+
+        //for(int lane = 0; lane < manager.data_MusicScore.Length; lane++)
+        //{
+        //    foreach(NotesInfo notes in manager.data_MusicScore[lane])
+        //    {
+        //        string str = "[" + lane + "," + ((int)notes.type) + "," + notes.bar + "." + notes.th + "/" + notes.barOriginTh + "]";
+        //        Debug.Log(str);
+        //        sw.WriteLine(str);
+        //    }
+        //}
+        sw.Close();
+
+
+        EditorApplication.delayCall += DestroyObj;
+    }
+
+    private void DestroyObj()
+    {
+        EditorApplication.delayCall -= DestroyObj;
+        DestroyImmediate(obj);
+    }
+}
