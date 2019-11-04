1a0a5a2e8d1d24fd6b2bb24e1f35506a341543b1
Hirokawa <17d3170021hm@edu.tech.ac.jp>
2019-11-04 15:16:37 +0900

    コンバート用のScriptableObject追加

---
+++ files/program/MusicGame_ TrialVersion/Assets/Scripts/ScriptableObject/ConvertSetting.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MusicGame/Call ConvertMenu", fileName = "ConvertMenu", order =-10)]
+public class ConvertSetting : ScriptableObject
+{
+    public TextAsset seetSource;
+    public DataType targetDataType;
+}
